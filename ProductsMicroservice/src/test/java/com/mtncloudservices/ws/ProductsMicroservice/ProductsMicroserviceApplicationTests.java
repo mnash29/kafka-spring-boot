cd56b205a870e59802d89badbaf9235c940b3ae0
mnash29 <mtntrades10@gmail.com>
2024-03-20 14:27:42 -0700

    Initial spring boot project

---
+++ ProductsMicroservice/src/test/java/com/mtncloudservices/ws/ProductsMicroservice/ProductsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtncloudservices.ws.ProductsMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
